df7b6ef1d9e55b1b85b10f6845ab06d1026f1b92
Flugel228 <imflugel@gmail.com>
2023-10-19 13:42:25 +0300

    feat: add  function in the Event model

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Event extends Model
 {
@@ -22,5 +23,10 @@ class Event extends Model
     public function creator(): BelongsTo
     {
         return $this->belongsTo(User::class, 'creator_id', 'id');
+    }
+
+    public function participants(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'event_participants', 'event_id', 'user_id');
     }
 }
