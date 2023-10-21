7c7dedaa883a46093f51a1917096710dead95e38
root <root@LAPTOP-8HCQLO2C>
2023-10-21 21:32:12 +0300

    feat: add vue.js

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['namespace' => 'App\Http\Controllers\Client'], function () {
+    Route::group(['namespace' => 'Admin'], function () {
+        Route::get('/', 'UserController@register')->name('admin.register');
+    });
 });
